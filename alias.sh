3848ee27254354137870b1f9a678a626f28915ad
Lysender <lysender@darkstar-win>
2025-03-29 11:41:33 +0800

    fix: cleanup redundant shortcuts

--- alias.sh
+++ alias.sh
@@ -1,6 +1,5 @@
 alias gff='git diff'
 alias gpl='git pull'
 alias gt='git status'
-alias gtc='git commit -m'
-alias gtd='git add . && git commit -m'
+alias gtc='git add . && git commit -m'
 alias gtp='git push'
